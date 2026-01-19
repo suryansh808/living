b2819d1b80301f866db333746df7dcd12ccc53a4
suryansh808 <suryanshsaxena808@gmail.com>
2026-01-19 14:52:17 +0530

    db.js file fixed

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,13 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-    });
-
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.error("MongoDB Connection Failed:", error.message);
-    process.exit(1); // Exit process on DB failure
+    process.exit(1);
   }
 };
 
